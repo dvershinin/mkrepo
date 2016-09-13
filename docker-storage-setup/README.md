07233ade2ba3f31adef0e8706cd194e895f669bb
Michal Kubenka <mkubenka@gmail.com>
2016-09-13 20:09:50 +0200

    Add docker-storage-setup.

---
+++ docker-storage-setup/README.md
@@ -0,0 +1,5 @@
+# docker-storage-setup
+
+[docker-storage-setup](https://github.com/projectatomic/docker-storage-setup) is a script to configure the devicemapper or overlayfs backends, part of the
+[Project Atomic](http://www.projectatomic.io/) suite of container projects. 
+
---
+++ docker-storage-setup/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+mkdir -p /tmp/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+echo '%_topdir /tmp/rpmbuild' > ~/.rpmmacros
+
+git clone https://github.com/projectatomic/docker-storage-setup /tmp/docker-storage-setup
+
+cd /tmp/docker-storage-setup
+
+cp /tmp/docker-storage-setup/docker-storage-setup.sh /tmp/rpmbuild/SOURCES
+cp /tmp/docker-storage-setup/docker-storage-setup.service /tmp/rpmbuild/SOURCES
+cp /tmp/docker-storage-setup/docker-storage-setup.conf /tmp/rpmbuild/SOURCES
+cp /tmp/docker-storage-setup/docker-storage-setup-override.conf /tmp/rpmbuild/SOURCES
+cp /tmp/docker-storage-setup/libdss.sh /tmp/rpmbuild/SOURCES
+
+sed -i "s|Version:.*|Version: %{version}|g" /tmp/docker-storage-setup/docker-storage-setup.spec
+sed -i "s|Release:.*|Release: %{release}|g" /tmp/docker-storage-setup/docker-storage-setup.spec
+
+COMMIT="$(git rev-parse --short HEAD)"
+VERSION="0.5"
+RELEASE="${ITERATION}.git${COMMIT}%{?dist}"
+
+rpmbuild \
+    --define="version ${VERSION}" \
+    --define="release ${RELEASE}" \
+    -bb \
+    /tmp/docker-storage-setup/docker-storage-setup.spec
+
+cp /tmp/rpmbuild/RPMS/x86_64/docker-storage-setup-${VERSION}*.rpm /tmp/fpmbuild
