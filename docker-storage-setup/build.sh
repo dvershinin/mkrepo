abcbe363a1e2b17e725e85b73faf4afaea977888
Michal Kubenka <mkubenka@gmail.com>
2016-09-27 11:34:36 +0200

    Fix conflict with docker-latest from extra repo. docker-storage-setup <= 0.5-3 is obsoleted by docker-latest and since I'm using different version convention it creates dependency problem.

--- docker-storage-setup/build.sh
+++ docker-storage-setup/build.sh
@@ -17,14 +17,17 @@ cp /tmp/docker-storage-setup/libdss.sh /tmp/rpmbuild/SOURCES
 
 sed -i "s|Version:.*|Version: %{version}|g" /tmp/docker-storage-setup/docker-storage-setup.spec
 sed -i "s|Release:.*|Release: %{release}|g" /tmp/docker-storage-setup/docker-storage-setup.spec
+sed -i "/Release:.*/a Epoch: %{epoch}" /tmp/docker-storage-setup/docker-storage-setup.spec
 
 COMMIT="$(git rev-parse --short HEAD)"
 VERSION="0.5"
 RELEASE="${ITERATION}.git${COMMIT}%{?dist}"
+EPOCH="1"
 
 rpmbuild \
     --define="version ${VERSION}" \
     --define="release ${RELEASE}" \
+    --define="epoch ${EPOCH}" \
     -bb \
     /tmp/docker-storage-setup/docker-storage-setup.spec
 
